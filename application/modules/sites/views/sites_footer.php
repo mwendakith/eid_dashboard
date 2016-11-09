1487f90e4202769ab829de67a4e698da8237c8d3
mwendakith <joelkith@gmail.com>
2016-11-09 10:20:17 +0300

    Edited sites default page

--- application/modules/sites/views/sites_footer.php
+++ application/modules/sites/views/sites_footer.php
@@ -2,28 +2,33 @@
 	$().ready(function(){
 		var site = <?php echo json_encode($this->session->userdata("site_filter")); ?>;
 		//$("#siteOutcomes").load("<?php echo base_url('charts/sites/site_outcomes');?>");
-		
-		if (!site) {
-    		$("#siteOutcomes").load("<?php echo base_url('charts/sites/site_outcomes');?>");
-			$("#first").show();
-			$("#second").hide();
-		} else {
-			$("#first").hide();
-			$("#second").show();
 
-			$("#tsttrends").html("<center><div class='loader'></div></center>");
-			$("#stoutcomes").html("<center><div class='loader'></div></center>");
-			$("#vlOutcomes").html("<center><div class='loader'></div></center>");
-			$("#ageGroups").html("<center><div class='loader'></div></center>");
+		// if (!site) {
+    // 		$("#siteOutcomes").load("<?php echo base_url('charts/sites/site_outcomes');?>");
+		// 	$("#first").show();
+		// 	$("#second").hide();
+		// } else {
+		// 	$("#first").hide();
+		// 	$("#second").show();
+		//
+		// 	$("#tsttrends").html("<center><div class='loader'></div></center>");
+		// 	$("#stoutcomes").html("<center><div class='loader'></div></center>");
+		// 	$("#vlOutcomes").html("<center><div class='loader'></div></center>");
+		// 	$("#ageGroups").html("<center><div class='loader'></div></center>");
+		//
+		// 	$("#tsttrends").load("<?php echo base_url('charts/sites/site_trends');?>/"+site);
+		// 	$("#stoutcomes").load("<?php echo base_url('charts/sites/site_positivity');?>/"+site);
+		// 	$("#vlOutcomes").load("<?php echo base_url('charts/sites/site_eid');?>/"+site);
+		// 	$("#ageGroups").load("<?php echo base_url('charts/sites/site_hei');?>/"+site);
+		//
+		// }
 
-			$("#tsttrends").load("<?php echo base_url('charts/sites/site_trends');?>/"+site);
-			$("#stoutcomes").load("<?php echo base_url('charts/sites/site_positivity');?>/"+site);
-			$("#vlOutcomes").load("<?php echo base_url('charts/sites/site_eid');?>/"+site);
-			$("#ageGroups").load("<?php echo base_url('charts/sites/site_hei');?>/"+site);
-			
-		}
+		$("#siteOutcomes").load("<?php echo base_url('charts/sites/site_outcomes');?>");
+		$("#first").show();
+		$("#second").hide();
 
-		
+
+
 		$("select").change(function() {
 			em = $(this).val();
 
@@ -34,7 +39,7 @@
 	        posting.done(function( data ) {
 	        	$.get("<?php echo base_url();?>template/dates", function(data){
 	        		obj = $.parseJSON(data);
-			
+
 					if(obj['month'] == "null" || obj['month'] == null){
 						obj['month'] = "";
 					}
@@ -63,7 +68,7 @@
 					$("#stoutcomes").load("<?php echo base_url('charts/sites/site_positivity');?>/"+data);
 					$("#vlOutcomes").load("<?php echo base_url('charts/sites/site_eid');?>/"+data);
 					$("#ageGroups").load("<?php echo base_url('charts/sites/site_hei');?>/"+data);
-					
+
 				}
 	        });
 		});
@@ -83,13 +88,13 @@
  		// Put the results in a div
 		posting.done(function( data ) {
 			obj = $.parseJSON(data);
-			
+
 			if(obj['month'] == "null" || obj['month'] == null){
 				obj['month'] = "";
 			}
 			$(".display_date").html("( "+obj['year']+" "+obj['month']+" )");
 			$(".display_range").html("( "+obj['prev_year']+" - "+obj['year']+" )");
-			
+
 		});
 		var site = <?php echo json_encode($this->session->userdata('site_filter')); ?>;
 		//Checking if site was previously selected and calling the relevant views
@@ -105,14 +110,14 @@
 			$("#stoutcomes").load("<?php echo base_url('charts/sites/site_positivity');?>/"+null+"/"+year);
 			$("#vlOutcomes").load("<?php echo base_url('charts/sites/site_eid');?>/"+null+"/"+year+"/"+month);
 			$("#ageGroups").load("<?php echo base_url('charts/sites/site_hei');?>/"+null+"/"+year+"/"+month);
-			
+
 		}
 		/*if(!$("#second").is(":hidden")){
 			alert('found');
 		}*/
 		///console.log(county);
 
-	 	
+
  	}
 
 	function ageModal()

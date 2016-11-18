e086792f2dd660a48dfdc24749f6a311eb78c385
bakasajoshua <baksajoshua09@gmail.com>
2016-11-18 17:07:45 +0300

    PHP Reporting

--- application/modules/charts/controllers/Charts.php
+++ application/modules/charts/controllers/Charts.php
@@ -19,5 +19,47 @@ class Charts extends MY_Controller {
 		
 	}
 
+	function excel_test()
+	{
+		$this->load->helper('download');
+        $this->load->library('PHPReport/PHPReport');
+
+		$data = array(array('ID' => 2,
+						'Name' => 'Joshua',
+						'Email' => 'baksajoshua@gmail.com',
+						'year' => 2016));
+
+		$template = 'Myexcel.xlsx';
+
+	    //set absolute path to directory with template files
+	    $templateDir = __DIR__ . "/";
+
+	    //set config for report
+	    $config = array(
+	        'template' => $template,
+	        'templateDir' => $templateDir
+	    );
+
+
+	      //load template
+	    $R = new PHPReport($config);
+	    
+	    $R->load(array(
+	            'id' => 'data',
+	            'repeat' => TRUE,
+	            'data' => $data   
+	        )
+	    );
+	      
+	      // define output directoy 
+	    $output_file_dir = __DIR__ ."/tmp/";
+	     // echo "<pre>";print_r("Still working");die();
+
+	    $output_file_excel = $output_file_dir  . "Myexcel.xlsx";
+	    //download excel sheet with data in /tmp folder
+	    // $result = $R->render('excel', $output_file_excel);
+	    force_download($output_file_excel, null);
+	}
+
 }
 ?>

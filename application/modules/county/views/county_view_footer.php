d70d05798d049a8db41ce28b44282a1e86dd8b02
bakasajoshua <baksajoshua09@gmail.com>
2016-11-23 18:08:38 +0300

    county page complete

--- application/modules/county/views/county_view_footer.php
+++ application/modules/county/views/county_view_footer.php
@@ -40,4 +40,50 @@
 	        });
 		});
 	});
+
+	function date_filter(criteria, id)
+ 	{
+ 		if (criteria === "monthly") {
+ 			year = null;
+ 			month = id;
+ 		}else {
+ 			year = id;
+ 			month = null;
+ 		}
+
+ 		var posting = $.post( '<?php echo base_url();?>template/filter_date_data', { 'year': year, 'month': month } );
+
+ 		// Put the results in a div
+		posting.done(function( data ) {
+			obj = $.parseJSON(data);
+			
+			if(obj['month'] == "null" || obj['month'] == null){
+				obj['month'] = "";
+			}
+			$(".display_date").html("( "+obj['year']+" "+obj['month']+" )");
+			$(".display_range").html("( "+obj['prev_year']+" - "+obj['year']+" )");
+
+			$.get("<?php echo base_url();?>county/check_county_select", function (data) {
+				if (data==0) {
+					$("#second").hide();
+					$("#first").show();
+				
+					// fetching the partner outcomes
+					$("#county_outcomes").html("<center><div class='loader'></div></center>");
+					$("#county_outcomes").load("<?php echo base_url('charts/summaries/county_outcomes');?>/"+year+"/"+month);
+				} else {
+					data = "<?php echo json_decode("+data+")?>";
+					$("#first").hide();
+					$("#second").show();
+					// Loader displaying
+		        	$("#subcounty_outcomes").html("<center><div class='loader'></div></center>");
+		        	$("#county_sites_details").html("<center><div class='loader'></div></center>");
+		        	// Actual graphs being loaded
+					$("#subcounty_outcomes").load("<?php echo base_url('charts/counties/subCounties_outcomes'); ?>/"+year+"/"+month+"/"+data);
+					$("#county_sites_details").load("<?php echo base_url('charts/counties/county_sites_details'); ?>/"+year+"/"+month+"/"+data);
+				}
+			});
+			
+		});
+	}
 </script>

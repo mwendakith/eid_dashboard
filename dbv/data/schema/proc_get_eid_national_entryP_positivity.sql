22eaf44f95775e69eb2f673ec34ecec42ab80f79
Bakasa <baksajoshua09@gmail.com>
2017-07-03 19:18:32 +0300

    Positivity changes

---
+++ dbv/data/schema/proc_get_eid_national_entryP_positivity.sql
@@ -0,0 +1,34 @@
+DROP PROCEDURE IF EXISTS `proc_get_eid_national_entryP_positivity`;
+DELIMITER //
+CREATE PROCEDURE `proc_get_eid_national_entryP_positivity`
+(IN filter_year INT(11), IN from_month INT(11), IN to_year INT(11), IN to_month INT(11))
+BEGIN
+  SET @QUERY =    "SELECT 
+  						`p`.`name`,
+                        SUM(`pos`) AS `pos`,
+                        SUM(`neg`) AS `neg`
+                    FROM `national_entrypoint` `ci` 
+                    LEFT JOIN `entry_points` `p` ON `ci`.`entrypoint` = `p`.`ID`
+                WHERE 1 ";
+
+   
+    IF (from_month != 0 && from_month != '') THEN
+      IF (to_month != 0 && to_month != '' && filter_year = to_year) THEN
+            SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' AND `month` BETWEEN '",from_month,"' AND '",to_month,"' ");
+        ELSE IF(to_month != 0 && to_month != '' && filter_year != to_year) THEN
+          SET @QUERY = CONCAT(@QUERY, " AND ((`year` = '",filter_year,"' AND `month` >= '",from_month,"')  OR (`year` = '",to_year,"' AND `month` <= '",to_month,"') OR (`year` > '",filter_year,"' AND `year` < '",to_year,"')) ");
+        ELSE
+            SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' AND `month`='",from_month,"' ");
+        END IF;
+    END IF;
+    ELSE
+        SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' ");
+    END IF;
+
+    SET @QUERY = CONCAT(@QUERY, " GROUP BY `p`.`ID` ");
+
+    PREPARE stmt FROM @QUERY;
+    EXECUTE stmt;
+    
+END //
+DELIMITER ;

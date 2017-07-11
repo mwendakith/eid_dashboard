b9288d2a7bbd3cbc681baf73717bda925bf8b5aa
mwendakith <joelkith@gmail.com>
2017-07-11 11:03:34 +0300

    Forgot to add files

---
+++ dbv/data/schema/proc_get_eid_counties_positivity_mixed.sql
@@ -0,0 +1,37 @@
+DROP PROCEDURE IF EXISTS `proc_get_eid_counties_positivity_mixed`;
+DELIMITER //
+CREATE PROCEDURE `proc_get_eid_counties_positivity_mixed`
+(IN filter_year INT(11), IN from_month INT(11), IN to_year INT(11), IN to_month INT(11))
+BEGIN
+  SET @QUERY =  "SELECT 
+                    `c`.`name`,
+                    SUM(`actualinfantsPOS`) AS `pos`,
+                    SUM(`actualinfants`-`actualinfantsPOS`) AS `neg`,
+                    SUM(`actualinfants`) AS `tests`,
+                    ((SUM(`actualinfantsPOS`)/(SUM(`actualinfants`)))*100) AS `pecentage`
+                FROM county_summary `cs`
+                LEFT JOIN countys `c`
+                    ON c.ID = cs.county 
+                WHERE 1";
+
+   
+    IF (from_month != 0 && from_month != '') THEN
+      IF (to_month != 0 && to_month != '' && filter_year = to_year) THEN
+            SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' AND `month` BETWEEN '",from_month,"' AND '",to_month,"' ");
+        ELSE IF(to_month != 0 && to_month != '' && filter_year != to_year) THEN
+          SET @QUERY = CONCAT(@QUERY, " AND ((`year` = '",filter_year,"' AND `month` >= '",from_month,"')  OR (`year` = '",to_year,"' AND `month` <= '",to_month,"') OR (`year` > '",filter_year,"' AND `year` < '",to_year,"')) ");
+        ELSE
+            SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' AND `month`='",from_month,"' ");
+        END IF;
+    END IF;
+    ELSE
+        SET @QUERY = CONCAT(@QUERY, " AND `year` = '",filter_year,"' ");
+    END IF;
+
+    SET @QUERY = CONCAT(@QUERY, " GROUP BY `c`.`name` ORDER BY `tests` DESC ");
+
+    PREPARE stmt FROM @QUERY;
+    EXECUTE stmt;
+    
+END //
+DELIMITER ;

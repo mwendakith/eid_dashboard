bfd4f6722596485a5a50931620a2be8e1ab1a145
Bakasa Joshua <baksajoshua09@gmail.com>
2018-09-05 10:56:00 +0000

    Correction of configs

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+	$system_path = '/var/www/eid_dir/eid_dashboard/system';
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	$application_folder = '/var/www/eid_dir/eid_dashboard/application';
 
 /*
  *---------------------------------------------------------------
